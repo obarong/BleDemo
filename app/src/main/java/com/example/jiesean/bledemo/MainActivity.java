96a2fdc16287d6075907fdf284ced4246e713f89
罗锦荣 <dustinljr@gmail.com>
2019-12-17 15:18:00 +0800

    添加停止扫描

--- app/src/main/java/com/example/jiesean/bledemo/MainActivity.java
+++ app/src/main/java/com/example/jiesean/bledemo/MainActivity.java
@@ -15,34 +15,46 @@ import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String TAG = "MainActivity";
 
     //目标设备的名称，可根据自己目标设备的不同去修改该名称来完成连接
-    private String mTargetDeviceName = "Mate9";
+    private String mTargetDeviceName = "honor band A1";
 
     private BluetoothManager mBluetoothManager;
     private BluetoothAdapter mBluetoothAdapter;
     private BluetoothLeScanner mBluetoothLeScanner;
     private List<BluetoothGattService> mServiceList;
     private ScanCallback mScanCallback;
+    private boolean mScanning;
+    private Handler handler;
+
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mStartScanBtn = (Button) findViewById(R.id.startScanBtn);
+        mStartScanBtn.setOnClickListener(this);
+
+        handler = new Handler();
 
         if (Build.VERSION.SDK_INT >= 21) {
             mScanCallback = new MyScanCallback();
@@ -75,17 +87,34 @@ public class MainActivity extends AppCompatActivity {
     /**
      * start Ble scan，扫描按钮的处理函数
      */
-    public void startScanLeDevices(View view) {
+    public void startScanLeDevices() {
         if (Build.VERSION.SDK_INT >= 18 && Build.VERSION.SDK_INT < 21) {
             //Android 4.3以上，Android 5.0以下
             mBluetoothAdapter.startLeScan(mLeScanCallback);
-
         } else if (Build.VERSION.SDK_INT >= 21) {
             //Android 5.0以上，扫描的结果在mScanCallback中进行处理
             mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
             mBluetoothLeScanner.startScan(mScanCallback);
-
         }
+        // Stops scanning after a pre-defined scan period.
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.e(TAG, "scan timeout");
+                stopScanLeDevices();
+            }
+        }, SCAN_PERIOD);
+        mScanning = true;
+    }
+
+    public void stopScanLeDevices() {
+        if (Build.VERSION.SDK_INT >= 18 && Build.VERSION.SDK_INT < 21) {
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        } else if (Build.VERSION.SDK_INT >= 21) {
+            mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+            mBluetoothLeScanner.stopScan(mScanCallback);
+        }
+        mScanning = false;
     }
 
     private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
@@ -94,11 +123,21 @@ public class MainActivity extends AppCompatActivity {
 
             Log.d(TAG, "BluetoothDevice  name=" + device.getName() + " address=" + device.getAddress());
             if (mTargetDeviceName.equals(device.getName())) {
+                handler.removeCallbacksAndMessages(null);
                 mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                device.connectGatt(MainActivity.this, false, mGattCallback);
             }
 
         }
     };
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.startScanBtn) {
+            startScanLeDevices();
+        }
+    }
 
     /**
      * LE设备扫描结果返回
@@ -115,11 +154,12 @@ public class MainActivity extends AppCompatActivity {
         public void onScanResult(int callbackType, ScanResult result) {
             //Log.d(TAG, "onScanResult");
             if (result != null) {
-                System.out.println("扫描到设备：" + result.getDevice().getName() + "  " + result.getDevice().getAddress());
+                Log.d(TAG, "扫描到设备：" + result.getDevice().getName() + "  " + result.getDevice().getAddress());
 
                 //此处，我们尝试连接MI 设备
                 if (result.getDevice().getName() != null && mTargetDeviceName.equals(result.getDevice().getName())) {
                     //扫描到我们想要的设备后，立即停止扫描
+                    handler.removeCallbacksAndMessages(null);
                     result.getDevice().connectGatt(MainActivity.this, false, mGattCallback);
                     mBluetoothLeScanner.stopScan(mScanCallback);
                 }
@@ -158,21 +198,28 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "onServicesDiscovered status" + status);
             mServiceList = gatt.getServices();
             if (mServiceList != null) {
-                System.out.println(mServiceList);
-                System.out.println("Services num:" + mServiceList.size());
+                Log.d(TAG, "mServiceList=" + mServiceList);
+                Log.d(TAG, "Services num:" + mServiceList.size());
             }
 
             for (BluetoothGattService service : mServiceList) {
                 List<BluetoothGattCharacteristic> characteristics = service.getCharacteristics();
-                System.out.println("扫描到Service：" + service.getUuid());
+                Log.d(TAG, "扫描到Service：" + service.getUuid());
 
                 for (BluetoothGattCharacteristic characteristic : characteristics) {
-                    System.out.println("characteristic: " + characteristic.getUuid());
+                    Log.d(TAG, "characteristic: " + characteristic.getUuid());
                 }
             }
-            Log.d(TAG, "onServicesDiscovered: readCharacteristic: " +
-                    gatt.readCharacteristic(gatt.getService(UUID.fromString("0000180F-0000-1000-8000-00805f9b34fb")).
-                            getCharacteristic(UUID.fromString("00002A19-0000-1000-8000-00805f9b34fb"))));
+            // 读取手机模拟BLE的特征值
+//            Log.d(TAG, "onServicesDiscovered: readCharacteristic: " +
+//                    gatt.readCharacteristic(gatt.getService(UUID.fromString("0000180F-0000-1000-8000-00805f9b34fb")).
+//                            getCharacteristic(UUID.fromString("00002A19-0000-1000-8000-00805f9b34fb"))));
+
+            // 调戏华为手环
+//            BluetoothGattCharacteristic temp = gatt.getService(UUID.fromString("6e400000-b5a3-f393-e0a9-e50e24dcca9d")).
+//                    getCharacteristic(UUID.fromString("6e400002-b5a3-f393-e0a9-e50e24dcca9d"));
+//            temp.setValue(new byte[]{63});
+//            gatt.writeCharacteristic(temp);
         }
 
         /**
@@ -255,5 +302,11 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, "onRequestPermissionsResult: 权限被用户拒绝");
             }
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopScanLeDevices();
     }
 }
